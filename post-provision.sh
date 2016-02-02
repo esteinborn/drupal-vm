4764d662827e5a6833b858e330634f9470123dbc
Justin Winter <Justin.Winter@troopers.state.nyenet>
2016-02-02 11:53:33 -0500

    post provision start

---
+++ post-provision.sh
@@ -0,0 +1,3 @@
+# TODO: Single command to setup, download database, etc
+# http://stackoverflow.com/questions/14124234/how-to-pass-parameter-on-vagrant-up-and-have-it-in-the-scope-of-chef-cookbook
+# drush @YOUR-ALIAS.dev sql-dump --structure-tables-list="hist*,cache*,*cache,sessions" | drush @drupalvm.drupalvm.dev sql-cli
