72efff9b2d7790fc33882c1e5eee6df3cc3bed49
Justin Winter <justinlevi@gmail.com>
2016-02-03 14:01:12 -0500

    global node modules and global composer packages

--- post-provision.sh
+++ post-provision.sh
@@ -1,3 +1,5 @@
 # TODO: Single command to setup, download database, etc
 # http://stackoverflow.com/questions/14124234/how-to-pass-parameter-on-vagrant-up-and-have-it-in-the-scope-of-chef-cookbook
 # drush @YOUR-ALIAS.dev sql-dump --structure-tables-list="hist*,cache*,*cache,sessions" | drush @drupalvm.drupalvm.dev sql-cli
+
+# When vtests/package.json exists, run npm install
